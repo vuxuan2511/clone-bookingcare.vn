10132fdf5bcb3ed92a0fca9505c6587f275f0290
vuxuan2511 <vuxuan183@gmail.com>
2022-09-10 14:21:22 +0700

    change files warning

--- src/axios.js
+++ src/axios.js
@@ -7,7 +7,7 @@ const instance = axios.create({
 });
 
 instance.interceptors.response.use((response) => {
-    const { data } = response;
+    // const { data } = response;
     return response.data;
 });
 
--- src/containers/System/RegisterPackageGroupOrAcc.js
+++ src/containers/System/RegisterPackageGroupOrAcc.js
@@ -1,33 +1,23 @@
 import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
+// import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 class RegisterPackageGroupOrAcc extends Component {
-
     constructor(props) {
         super(props);
-
+        this.state = {};
     }
-
-
 
     render() {
-        return (
-            <div className="text-center">
-                register package group or account
-            </div>)
+        return <div className="text-center">register package group or account</div>;
     }
-
 }
 
-const mapStateToProps = state => {
-    return {
-
-    };
+const mapStateToProps = (state) => {
+    return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(RegisterPackageGroupOrAcc);
